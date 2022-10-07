58dc5f9ebcee7bf6193b75de611c4688a2106d99
Junichi Nishi <github@purewest.dip.jp>
2022-10-07 23:15:33 +0900

    docker-composeを修正

--- First_Project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# プロジェクトを新規作成
-# docker-compose -f <指定するdocker-composeのファイル> run app django-admin startproject <プロジェクト名> <プロジェクトを作成するディレクトリ>
-
-# 例：
-docker-compose run app django-admin startproject django_pj .
